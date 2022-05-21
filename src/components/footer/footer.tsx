33f84b9ec7a11d974982839aca8af63c8a2ec18b
Robert Garcia <rgs2@icomp.ufam.edu.br>
2022-05-21 10:44:36 -0400

    feat(footer): create footer component

---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,9 @@
+import { Flex, Text } from '@chakra-ui/react';
+
+export function Footer() {
+	return (
+		<Flex as="footer" p="4" bg="gray.100">
+			<Text>Coltech</Text>
+		</Flex>
+	);
+}
--- src/routes/home/home.tsx
+++ src/routes/home/home.tsx
@@ -1,12 +1,19 @@
+import { Flex } from '@chakra-ui/react';
 import { Helmet } from 'react-helmet-async';
+
+import { Footer } from '../../components/footer/footer';
 
 function Home() {
 	return (
-		<main>
-			<Helmet>
-				<title>Coltech | Home</title>
-			</Helmet>
-		</main>
+		<Flex h="100vh" direction="column">
+			<main>
+				<Helmet>
+					<title>Coltech | Home</title>
+				</Helmet>
+				<h1>jdfadfjads</h1>
+			</main>
+			<Footer />
+		</Flex>
 	);
 }
 
