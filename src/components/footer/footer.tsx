64ebe7eabb3265046992876f9a0a15c2f53b2b97
Robert Garcia <rgs2@icomp.ufam.edu.br>
2022-05-21 16:58:57 -0400

    chore(footer): add icons and description of the footer
    Has banner component was revised by lint

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,4 +1,21 @@
-import { Flex, Image, ListItem, UnorderedList } from '@chakra-ui/react';
+import {
+	Box,
+	Flex,
+	Grid,
+	GridItem,
+	Icon,
+	Image,
+	ListItem,
+	Text,
+	UnorderedList,
+} from '@chakra-ui/react';
+import {
+	FaInstagram,
+	FaFacebook,
+	FaWhatsapp,
+	FaSpotify,
+	FaYoutube,
+} from 'react-icons/fa';
 
 import { Button } from './button';
 
@@ -10,12 +27,13 @@ export function Footer() {
 			p="4"
 			bg="gray.100"
 			alignItems="center"
+			borderTopRadius="30"
 		>
 			<Image
 				src="src/assets/capa_coltech_laranja_cinza.png"
 				alt="logo coltech completo"
-				w="32"
-				h="8"
+				h="10"
+				mx="auto"
 			/>
 			<UnorderedList
 				color="gray.500"
@@ -23,6 +41,7 @@ export function Footer() {
 				textAlign="center"
 				fontSize="16px"
 				my="4"
+				mx="auto"
 			>
 				<ListItem>Home</ListItem>
 				<ListItem>Sobre Nós</ListItem>
@@ -32,6 +51,31 @@ export function Footer() {
 			</UnorderedList>
 
 			<Button name="Orçamento" />
+
+			<Grid templateColumns="repeat(5, 1fr)" gap={3} mx="auto" my="4">
+				<GridItem>
+					<Icon as={FaInstagram} w="6" h="6" color="gray.500" />
+				</GridItem>
+				<GridItem>
+					<Icon as={FaFacebook} w="6" h="6" color="gray.500" />
+				</GridItem>
+				<GridItem>
+					<Icon as={FaWhatsapp} w="6" h="6" color="gray.500" />
+				</GridItem>
+				<GridItem>
+					<Icon as={FaSpotify} w="6" h="6" color="gray.500" />
+				</GridItem>
+				<GridItem>
+					<Icon as={FaYoutube} w="6" h="6" color="gray.500" />
+				</GridItem>
+			</Grid>
+
+			<Box textAlign="center" width="80%">
+				<Text fontSize="12px" color="gray.500">
+					Desenvolvido por Arco - Consultoria em Arquitetura e Design e Coltech
+					Consultoria
+				</Text>
+			</Box>
 		</Flex>
 	);
 }
