2810dcf16a966594ae1a8c5c37a13d39674e1c58
grvela <rodrigo@vela.localdomain>
2022-05-20 22:14:24 -0400

    feat(structure): components and routes was added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import { Helmet } from 'react-helmet';
+import { Route, Routes } from 'react-router-dom';
+import Home from './routes/home/home';
 
 function App() {
 	return (
-		<Helmet>
-			<title>Coltech | Home</title>
-		</Helmet>
+		<main>
+			<Helmet>
+				<title>Coltech | Home</title>
+			</Helmet>
+			<Routes>
+				<Route path="/" element={<Home />} />
+			</Routes>
+		</main>
 	);
 }
 
--- src/routes/home/home.tsx
+++ src/routes/home/home.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 function Home() {
 	return <p>Home</p>;
 }
