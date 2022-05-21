af6d417ad75320721ffc11e5b109667e8bb07b57
Robert Garcia <rgs2@icomp.ufam.edu.br>
2022-05-21 13:59:59 -0400

    chore(footer): add list of pages, button and coltech logo

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,11 +1,37 @@
-import { Flex, Text, Box } from '@chakra-ui/react';
+import { Flex, Image, ListItem, UnorderedList } from '@chakra-ui/react';
+
+import { Button } from './button';
 
 export function Footer() {
 	return (
-		<Flex as="footer" p="4" bg="gray.100">
-			<Text>Coltech</Text>
+		<Flex
+			as="footer"
+			direction="column"
+			p="4"
+			bg="gray.100"
+			alignItems="center"
+		>
+			<Image
+				src="src/assets/capa_coltech_laranja_cinza.png"
+				alt="logo coltech completo"
+				w="32"
+				h="8"
+			/>
+			<UnorderedList
+				color="gray.500"
+				styleType="none"
+				textAlign="center"
+				fontSize="16px"
+				my="4"
+			>
+				<ListItem>Home</ListItem>
+				<ListItem>Sobre Nós</ListItem>
+				<ListItem>Serviços</ListItem>
+				<ListItem>Portfólio</ListItem>
+				<ListItem>Contato</ListItem>
+			</UnorderedList>
 
-			<Box p="4" />
+			<Button name="Orçamento" />
 		</Flex>
 	);
 }
