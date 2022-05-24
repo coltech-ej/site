1e55e75b65d8a60e05e65650cb2632e0c9630cd9
Robert Garcia <rgs2@icomp.ufam.edu.br>
2022-05-24 16:08:50 -0400

    feat: create services part
    create a card-service component and slides on page

--- src/routes/home/home.tsx
+++ src/routes/home/home.tsx
@@ -1,8 +1,18 @@
 import { Flex } from '@chakra-ui/react';
 import { Helmet } from 'react-helmet-async';
+import { Navigation } from 'swiper';
+// eslint-disable-next-line import/no-unresolved
+import { Swiper, SwiperSlide } from 'swiper/react';
 
-import { Footer, Header } from '../../components';
+import { Footer, Header, Title } from '../../components';
 import { Banner } from '../../components/banner/banner';
+
+// Import Swiper styles
+// eslint-disable-next-line import/no-unresolved
+import 'swiper/css';
+// eslint-disable-next-line import/no-unresolved
+import 'swiper/css/navigation';
+import '../../styles/styles.scss';
 
 function Home() {
 	return (
@@ -15,6 +25,27 @@ function Home() {
 				<Header />
 
 				<Banner />
+
+				<Title name="Nossos Serviços" />
+
+				<Flex w="100%" h="xs">
+					<Swiper
+						slidesPerView={1}
+						spaceBetween={30}
+						loop
+						pagination={{
+							clickable: true,
+						}}
+						navigation
+						modules={[Navigation]}
+						className="mySwiper"
+					>
+						<SwiperSlide>Slide 1</SwiperSlide>
+						<SwiperSlide>Slide 2</SwiperSlide>
+						<SwiperSlide>Slide 3</SwiperSlide>
+						<SwiperSlide>Slide 4</SwiperSlide>
+					</Swiper>
+				</Flex>
 			</main>
 
 			<Footer />
