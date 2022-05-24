e8eaaee34220934bdcfadab8a4b0e74a00b1f1a2
Robert Garcia <rgs2@icomp.ufam.edu.br>
2022-05-24 17:26:06 -0400

    chore(button): change button and new config eslint

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,3 +1,5 @@
 export { Title } from './title/title';
 export { Footer } from './footer/footer';
 export { Header } from './header/header';
+export { Button } from './button';
+export { InfoCorp } from './info-corp';
