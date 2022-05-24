9f69d0e0c8550d3759202023073c1fadda115774
Robert Garcia <rgs2@icomp.ufam.edu.br>
2022-05-24 17:30:04 -0400

    style(home): styled about us in the home

--- src/routes/home/home.tsx
+++ src/routes/home/home.tsx
@@ -1,9 +1,9 @@
 import { Flex, Text, Grid, GridItem } from '@chakra-ui/react';
 import { Helmet } from 'react-helmet-async';
 
-import { Footer, Header, Title } from '../../components';
+import { Footer, Header, Title, Button, InfoCorp } from '../../components';
 import { Banner } from '../../components/banner/banner';
-import { Button } from '../../components/footer/button';
+
 function Home() {
 	return (
 		<Flex h="100vh" direction="column">
@@ -16,30 +16,39 @@ function Home() {
 
 				<Banner />
 
-				<Flex
-					background='gray.100'
-					direction='column'
-					padding='5'
-				>
-					<Title name="Sobre nós" />
-					<Text color='gray.500'>
-						Fundado no dia 26 de abril de 2016, a Coltech é uma Empresa Júnior de Consultoria em Engenharia formada por alunos e professores da UFAM e vinculada à instituição.
+				<Flex background="gray.100" direction="column" padding="5">
+					<Title name="Sobre nós" color="gray.500" />
 
-						A partir de jovens com necessidade de ter experiências profissionais durante a jornada na universidade e conhecer o mercado de trabalho.
+					<Text color="gray.500" textAlign="justify" mt="3">
+						Fundado no dia 26 de abril de 2016, a Coltech é uma Empresa Júnior
+						de Consultoria em Engenharia formada por alunos e professores da
+						UFAM e vinculada à instituição. <br /> A partir de jovens com
+						necessidade de ter experiências profissionais durante a jornada na
+						universidade e conhecer o mercado de trabalho.
 					</Text>
-					<Button name="Saiba mais" />
-					<Grid>
-						<GridItem>
-							60
+
+					<Button w="28" mt="3" alignSelf="center" borderRadius="20px">
+						Saiba mais
+					</Button>
+
+					<Grid
+						w="100%"
+						templateColumns="repeat(2, 1fr)"
+						gap={2}
+						mt="4"
+						mx="auto"
+					>
+						<GridItem color="gray.500">
+							<InfoCorp number="60K" type="faturamento" />
 						</GridItem>
-						<GridItem>
-							40
+						<GridItem color="gray.500">
+							<InfoCorp number="30" type="projetos" />
 						</GridItem>
-						<GridItem>
-							20
+						<GridItem color="gray.500">
+							<InfoCorp number="12" type="membros" />
 						</GridItem>
-						<GridItem>
-							10
+						<GridItem color="gray.500">
+							<InfoCorp number="12" type="projetos de impacto" />
 						</GridItem>
 					</Grid>
 				</Flex>
